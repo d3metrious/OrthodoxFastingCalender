a0b08fbdc8205d1d5f669154ad5e4c1ac3b332b1
d3metrious <jimi.demetriou@gmail.com>
2026-03-12 16:45:57 -0400

    arabic translations

--- lib/utils/translations.dart
+++ lib/utils/translations.dart
@@ -66,21 +66,81 @@ class GreekStrings extends AppStrings {
 class ArabicStrings extends AppStrings {
   @override String get today => 'اليوم';
   @override String get settings => 'الإعدادات';
+  @override String get theme => 'المظهر';
+  @override String get light => 'وضع فاتح';
+  @override String get dark => 'وضع داكن';
+  @override String get system => 'تلقائي';
+  @override String get font => 'الخط';
+  @override String get language => 'اللغة';
+  @override String get month => 'الشهر';
+  @override String get day => 'اليوم';
+  @override String get selectDay => 'اختر يوماً لرؤية تفاصيل الصيام.';
+  @override String get noFasting => 'لا يوجد صيام';
+  @override String get noFastingDesc => 'جميع الأطعمة مسموحة اليوم.';
+  @override String get fastingRules => 'قوانين الصيام';
+  @override String get fastingEncouragement => 'في هذا اليوم، يُشجع المؤمنون على اتباع إرشادات الصيام التقليدية حسب تقاليدهم.';
   @override String get strictFastLabel => 'صوم انقطاعي';
+  @override String get wineOilLabel => 'نبيذ وزيت';
+  @override String get fishOilWineLabel => 'سمك وزيت ونبيذ';
+  @override String get dairyAllowedLabel => 'يسمح بمنتجات الألبان';
+  @override String get strictFastDesc => 'الامتناع عن اللحم والسمك والزيت والنبيذ والألبان والبيض.';
+  @override String get wineOilDesc => 'يسمح بالنبيذ والزيت. الامتناع عن اللحم والسمك والألبان والبيض.';
+  @override String get fishOilWineDesc => 'يسمح بالسمك والزيت والنبيذ. الامتناع عن اللحم والألبان والبيض.';
+  @override String get dairyAllowedDesc => 'يسمح بمنتجات الألبان والبيض والسمك والزيت والنبيذ. الامتناع عن اللحم.';
 }
 
 /// Church Slavonic Overrides
 class SlavonicStrings extends AppStrings {
   @override String get today => 'Днесь';
   @override String get settings => 'Настройки';
+  @override String get theme => 'Оформленіе';
+  @override String get light => 'Свѣтлая';
+  @override String get dark => 'Темная';
+  @override String get system => 'По умолчанию';
+  @override String get font => 'Шрифтъ';
+  @override String get language => 'Языкъ';
+  @override String get month => 'Мѣсяцъ';
+  @override String get day => 'День';
+  @override String get selectDay => 'Выбери день да видиши правила поста.';
+  @override String get noFasting => 'Поста нѣсть';
+  @override String get noFastingDesc => 'Всяка пища разрѣшена днесь.';
+  @override String get fastingRules => 'Постныя правила';
+  @override String get fastingEncouragement => 'Въ сей день вѣрніи призываются соблюдати традиціонная правила поста по преданію ихъ.';
   @override String get strictFastLabel => 'Строгій постъ';
+  @override String get wineOilLabel => 'Вино и елей';
+  @override String get fishOilWineLabel => 'Рыба, вино и елей';
+  @override String get dairyAllowedLabel => 'Разрѣшеніе на вся';
+  @override String get strictFastDesc => 'Воздержаніе отъ мяса, рыбы, елея, вина, млечныхъ и яицъ.';
+  @override String get wineOilDesc => 'Вино и елей разрѣшаются. Воздержаніе отъ мяса, рыбы, млечныхъ и яицъ.';
+  @override String get fishOilWineDesc => 'Рыба, елей и вино разрѣшаются. Воздержаніе отъ мяса, млечныхъ и яицъ.';
+  @override String get dairyAllowedDesc => 'Млечная, яйца, рыба, елей и вино разрѣшаются. Воздержаніе отъ мяса.';
 }
 
 /// Ethiopian (Amharic) Overrides
 class AmharicStrings extends AppStrings {
   @override String get today => 'ዛሬ';
   @override String get settings => 'ቅንጅቶች';
+  @override String get theme => 'ገጽታ';
+  @override String get light => 'ብሩህ ገጽታ';
+  @override String get dark => 'ጨለማ ገጽታ';
+  @override String get system => 'የስርዓቱ መደበኛ';
+  @override String get font => 'ፊደል';
+  @override String get language => 'ቋንቋ';
+  @override String get month => 'ወር';
+  @override String get day => 'ቀን';
+  @override String get selectDay => 'የጾም ዝርዝሮችን ለማየት ቀኑን ይምረጡ።';
+  @override String get noFasting => 'ጾም የለም';
+  @override String get noFastingDesc => 'ዛሬ ሁሉም ምግቦች ይፈቀዳሉ።';
+  @override String get fastingRules => 'የጾም ህጎች';
+  @override String get fastingEncouragement => 'በዚህ ቀን ምዕመናን እንደ ወጋቸው ባህላዊውን የጾም መመሪያ እንዲከተሉ ይበረታታሉ።';
   @override String get strictFastLabel => 'አጽዋማት';
+  @override String get wineOilLabel => 'ወይን እና ዘይት';
+  @override String get fishOilWineLabel => 'ዓሳ፣ ዘይት እና ወይን';
+  @override String get dairyAllowedLabel => 'የወተት ተዋጽኦ ይፈቀዳል';
+  @override String get strictFastDesc => 'ከሥጋ፣ ከዓሳ፣ ከዘይት፣ ከወይን፣ ከወተት እና ከእንቁላል መከልከል::';
+  @override String get wineOilDesc => 'ወይን እና ዘይት ይፈቀዳሉ። ከሥጋ፣ ከዓሳ፣ ከወተት እና ከእንቁላል መከልከል::';
+  @override String get fishOilWineDesc => 'ዓሳ፣ ዘይት እና ወይን ይፈቀዳሉ። ከወተት እና ከእንቁላል መከልከል::';
+  @override String get dairyAllowedDesc => 'የወተት ተዋጽኦ፣ እንቁላል፣ ዓሳ፣ ዘይት እና ወይን ይፈቀዳሉ። ከሥጋ መከልከል::';
 }
 
 class Translations {
